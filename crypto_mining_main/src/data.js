65926638bd5784b7f624b2e3de039e453d530b8a
Sandaru Vihanga <109741445+sandaruvk@users.noreply.github.com>
2024-03-28 00:05:53 +0530

    Added names and reviews

--- crypto_mining_main/src/data.js
+++ crypto_mining_main/src/data.js
@@ -1,7 +1,7 @@
 const reviews = [
   {
     id: 1,
-    name: "Md. Jihad",
+    name: "Md. Lathma Piyathilaka",
     job: "Full-Stack Developer",
     image:
       "",
@@ -9,21 +9,21 @@ const reviews = [
   },
   {
     id: 2,
-    name: "Lotifa Akan Anannya",
+    name: "Pamudi Nannayakkara",
     job: "System Analyst",
     image: "",
     text: "I have been working with them for several projects now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
   },
   {
     id: 3,
-    name: "Ab. Rahim Ahmed",
+    name: "Sandaru Vihanga",
     job: "Project Manager",
     image: "",
     text: "I am thrilled with the results of our hard work, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
   },
   {
     id: 4,
-    name: "Nur Adnan Chowdhury",
+    name: "Chrismi Kotikawatta",
     job: "Full-Stack Developer",
     image:
       "",
@@ -31,7 +31,7 @@ const reviews = [
   },
   {
     id: 5,
-    name: "Most. Mahbuba Yasmin",
+    name: "Imeth Wijayrathna",
     job: "Marketing Manager",
     image:
       "",
@@ -39,7 +39,7 @@ const reviews = [
   },
   {
     id: 6,
-    name: "Salma Jahan",
+    name: "Anuda B",
     job: "Assistent System Analyst",
     image:
       "",
