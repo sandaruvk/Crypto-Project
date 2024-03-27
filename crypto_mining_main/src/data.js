7deb3befd884342beb5898b7ce1ecbad6ddfe90f
Sandaru Vihanga <109741445+sandaruvk@users.noreply.github.com>
2024-03-28 00:03:42 +0530

    added the pages, assets and the packages

---
+++ crypto_mining_main/src/data.js
@@ -0,0 +1,50 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Md. Jihad",
+    job: "Full-Stack Developer",
+    image:
+      "",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Lotifa Akan Anannya",
+    job: "System Analyst",
+    image: "",
+    text: "I have been working with them for several projects now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Ab. Rahim Ahmed",
+    job: "Project Manager",
+    image: "",
+    text: "I am thrilled with the results of our hard work, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Nur Adnan Chowdhury",
+    job: "Full-Stack Developer",
+    image:
+      "",
+    text: "I have worked with many developers over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Most. Mahbuba Yasmin",
+    job: "Marketing Manager",
+    image:
+      "",
+    text: "I had the opportunity to work with this team as a marketing manager with this project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 6,
+    name: "Salma Jahan",
+    job: "Assistent System Analyst",
+    image:
+      "",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
